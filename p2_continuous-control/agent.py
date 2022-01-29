017487f5c30f8f3f7e46aad7156db126162bf361
Ocio <socio@infinityward.com>
2022-01-28 17:27:15 -0800

    First version that compiles and runs

--- p2_continuous-control/agent.py
+++ p2_continuous-control/agent.py
@@ -1,5 +1,4 @@
-from replay_buffer import ReplayBuffer
-from model import QNetwork
+from model import ActorCritic
 
 import numpy as np
 import random
@@ -7,108 +6,54 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-class Agent():
+class Agent:
+    def __init__( self, state_size, action_size, actor_layer_size, critic_layer_size, gamma ):
+        self.local_actor_critic = ActorCritic( state_size, action_size, actor_layer_size, critic_layer_size, gamma )
 
-    def __init__(self, state_size, action_size, seed, device, buffer_size = int(1e5), batch_size = 64, gamma = 0.99, tau = 1e-3, learning_rate=5e-4, update_every = 4):
-        """Initializes the agent
-        
-        Params
-        ======
-            state_size (int): number of parameters in the state
-            action_size (int): number of available actions
-            seed (int): random seed
-            buffer_size (int): size of the guy's memory buffer
-            batch_size (int): size of the batch when we get a random sample from the memory
-            gamma (float): discount factor
-            tau (float): used for soft update
-            learning_rate (float): learning rate for the optimizer
-            update_every (int): how many steps before we get a new batch from the memory
-        """
+class AgentOverlord:
+
+    def __init__( self, state_size, action_size, agent_num, actor_layer_size=128, critic_layer_size = 128, gamma = 0.99, learning_rate=5e-4 ):
         self.state_size = state_size
         self.action_size = action_size
-        self.seed = random.seed(seed)
-        self.update_every = update_every
-        self.device = device
-
-        #-- Hyperparameters
-        self.batch_size = batch_size
+        
+        #-- We're solving this problem using A3C, which means we need a global actor critic (that will be shared
+        #   among every agent), and we'll have a local actor critic per agent
+        self.global_actor_critic = ActorCritic( state_size, action_size, actor_layer_size, critic_layer_size, gamma )
+        self.agents = [ Agent( state_size, action_size, actor_layer_size, critic_layer_size, gamma ) for _ in range( agent_num ) ]
+        self.scores = np.zeros( agent_num ) 
+        
+        #-- Store gamma and initialize optimizer. The optimizer is shared between all of the agents
         self.gamma = gamma
-        self.tau = tau
-        # learning_rate doesn't need to be stored, as it's not being used outside this function
-
-        #-- We're solving this problem by using Deep Q Learning. That means...
-
-        # 1) We have two networks: local and target
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=learning_rate)
-
-        # 2) Initialize replay memory
-        self.memory = ReplayBuffer(action_size, seed, buffer_size, batch_size)
-        self.t_step = 0
-
-
-    def step(self, state, action, reward, next_state, done):
-        """ Updates the agent after an action is taken """
+        self.optimizer = optim.Adam( self.global_actor_critic.parameters(), lr = learning_rate )
         
-        # Remember this step
-        self.memory.add(state, action, reward, next_state, done)
-
-        # Decide whether we need to get another batch of samples from the memory
-        if self.t_step == 0:
-            if len(self.memory) > self.batch_size:
-                experiences = self.memory.sample(self.device)
-                self.learn(experiences)
         
-        self.t_step += 1
-        if self.t_step >= self.update_every:
-            self.t_step = 0
-
-    def learn(self, experiences):
-        """ Updates value parameters using the batch of experiences"""
-        states, actions, rewards, next_states, dones = experiences
-
-        # 1) Get predicted Q targets
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-
-        # 2) Calculate Q targets
-        Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
-
-        # 3) Get expected Q from local model
-        Q_expected = self.qnetwork_local(states).gather(1, actions) 
-
-        # 4) Compute loss
-        loss = F.mse_loss(Q_expected, Q_targets)
-
-        # 5) And minimize the loss
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
-        # 6) Finally, update target network with the weights calculated so far for the local network
-        self.soft_update(self.qnetwork_local, self.qnetwork_target)
-    
-    def act(self, state, epsilon=0.0):
-        """ Selects an action to take from the current state using the current policy """
-        # Transform state
-        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+    def step( self, states, actions, rewards, next_states, dones ):				
+        self.scores += rewards
         
-        # Get current action values
-        self.qnetwork_local.eval()
-        with torch.no_grad():
-            action_values = self.qnetwork_local(state)
-        self.qnetwork_local.train()
+        for observation, agent in zip( zip( states, actions, rewards, next_states, dones ), self.agents ):
+            agent.local_actor_critic.memory.remember( observation[ 0 ], observation[ 1 ], observation[ 2 ] )
 
-        # Epsilon-greedy action selection (i.e. random choice instead of following policy with some probabilily)
-        if random.random() > epsilon:
-            return np.argmax(action_values.cpu().data.numpy())
-        else:
-            return random.choice(np.arange(self.action_size))
-
-    def soft_update(self, local_model, target_model):
-        """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-        """
-        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(self.tau * local_param.data + (1.0 - self.tau) * target_param.data)
+            loss = agent.local_actor_critic.calculate_loss( observation[ 4 ] )
+            self.optimizer.zero_grad()
+            loss.backward()
+            for local_param, global_param in zip( agent.local_actor_critic.parameters(), self.global_actor_critic.parameters() ):
+                global_param.grad = local_param.grad
+            
+            self.optimizer.step()
+            
+            # synchronize with global
+            agent.local_actor_critic.load_state_dict( self.global_actor_critic.state_dict() )
+            
+            # clear local memory after every step
+            agent.local_actor_critic.memory.clear()
+        
+        
+    def choose_actions( self, states ):
+        actions = []
+        for agent, state in zip( self.agents, states ):
+            action = agent.local_actor_critic.choose_action( state )
+            actions.append(action.tolist() )
+        
+        return actions
+ 
 
--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -1,41 +1,133 @@
-import torch
+import torch as T
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions import Categorical
+from torch.distributions import Normal
 
-class QNetwork(nn.Module):
+#-- MEMORY USED BY THE MODEL
+class ModelMemory:
+    def __init__(self):
+        self.clear()
+    
+    def clear(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        
+    def remember(self, state, action, reward):
+        self.states.append( state )
+        self.actions.append( action )
+        self.rewards.append( reward ) 
+        
+    def get_states( self ):
+        return T.tensor( self.states, dtype=T.float )
+    
+    def get_actions( self ):
+        return T.tensor( self.actions, dtype=T.float )
+        
+    def calculate_batch_R( self, R, gamma ):
+        # from "Multicore Deep Reinforcement Learning - https://www.youtube.com/watch?v=OcIx_TBu90Q"
+        batch_return = []
+        for reward in self.rewards[::-1]:
+            R = reward + gamma * R
+            batch_return.append( R )
+            
+        # we need to reverse the order of the array to get them in the same order as they were seen
+        batch_return.reverse()
+        
+        return T.tensor( batch_return, dtype=T.float )
 
-    def __init__(self, state_size, action_size, seed, hidden_dims=(64, 64), activation_func=F.relu):
-        super(QNetwork, self).__init__()
+#-- MODEL
+class ActorCritic(nn.Module):
 
-        self.activation_func = activation_func
-        self.seed = torch.manual_seed(seed)
+    def __init__(self, input_size, action_size, actor_layer_size=128, critic_layer_size = 128, gamma=0.99):
+        super(ActorCritic, self).__init__()
 
-        # Initialize input layer
-        self.input_layer = nn.Linear(state_size, hidden_dims[0])
+        self.base = nn.Sequential( nn.Linear( input_size, actor_layer_size ), 
+                                    nn.ReLU())
 
-        # Create as many hidden layers as requested in the constructor
-        self.hidden_layers = nn.ModuleList();
-        for i in range(len(hidden_dims) - 1):
-            new_hidden_layer = nn.Linear(hidden_dims[i], hidden_dims[i+1])
-            self.hidden_layers.append(new_hidden_layer)
+        # Initialize actor network
+        # -- This network will calculate the policy
+        #    Our actions are continuous, so we will use a Normal distribution to sample them
+        #    To do that, we want to calculate mean and standard variation for each element of the action
+        self.actorMu = nn.Sequential( nn.Linear( actor_layer_size, action_size ), nn.Tanh() )  #use tanh since the values can be -1..1
+        self.actorSigma = nn.Sequential( nn.Linear( actor_layer_size, action_size ), nn.Softplus() ) #softplus will allow some negative values to be transformed to positive
+        
+        # Initialize critic network
+        # -- This network will calculate the value of the actions chosen by the actor
+        self.critic = nn.Linear( critic_layer_size, 1 )
 
-        # Create output layer
-        self.output_layer = nn.Linear(hidden_dims[-1], action_size)
+        # Memory
+        self.memory = ModelMemory()
+        
+        # Hyperparameters
+        self.gamma = gamma
+        
+        self.action_size = action_size
 
-    def forward(self, state):
+    def forward( self, state ):
+        # first, map inputs to hidden layer
+        base_out = self.base( state )
+    
+        # update actor
+        mu = self.actorMu( base_out )
+        sigma = self.actorSigma( base_out )
 
-        # Send our state through the net...
-        # 1) input layer
-        x = self.activation_func(self.input_layer(state))
+        # update critic
+        v = self.critic( base_out )
+    
+        return mu, sigma, v
+        
+    def calculate_R( self, done ):
+        states = self.memory.get_states()
+        _, _, v = self.forward( states )
 
-        # 2) hidden layers
-        for hidden_layer in self.hidden_layers:
-            x = self.activation_func(hidden_layer(x))
+        # value of the last step, or zero if it's a terminal state
+        R = 0 if done else v[ -1 ]
 
-        # 3) output layer. Don't apply the activation function here
-        x = self.output_layer(x)
+        batch_return = self.memory.calculate_batch_R( R, self.gamma )
+        return batch_return
+        
+    def calculate_loss( self, done ):
+        states = self.memory.get_states()
+        actions = self.memory.get_actions()
+        returns = self.calculate_R( done )
+        
+        mu, sigma, values = self.forward( states )
+        
+        values = values.squeeze()
+        critic_loss = ( returns - values ) ** 2
+ 
+        distribution = Normal( mu, sigma )
+        log_probabilities = distribution.log_prob( actions )
+        actor_loss = -log_probabilities * ( returns - values )
 
-        # We're taking a state and converting it to action values
-        # by making the original input go through every layer of the network
-        return x
+        total_loss = ( critic_loss + actor_loss ).mean()
+        return total_loss
+            
+    def choose_action( self, state ):
+        state = T.tensor( [state], dtype= T.float )
+        mu, sigma, _ = self.forward( state )
 
+        distribution = Normal( mu, sigma )
+        action = distribution.sample()[0].numpy()
+        action = np.clip(action, -1, 1)
+
+        return action
+
+    def step(self, state, action, reward, next_state, done):
+        """ Updates the agent after an action is taken """
+        
+        # Remember this step
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Decide whether we need to get another batch of samples from the memory
+        if self.t_step == 0:
+            if len(self.memory) > self.batch_size:
+                experiences = self.memory.sample(self.device)
+                self.learn(experiences)
+        
+        self.t_step += 1
+        if self.t_step >= self.update_every:
+            self.t_step = 0
